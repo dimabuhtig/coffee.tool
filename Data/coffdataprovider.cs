24a9faaacb52fa34c82fb1d070bcf0c38dfcbeec
dimabuhtig <qa@qaqa.org>
2025-04-02 16:39:47 +0300

    add dataporovidor

---
+++ Data/coffdataprovider.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+using coffee.tool.Model;
+
+namespace coffee.tool.Data;
+
+public class coffdataprovider
+{
+    public IEnumerable<CofeeShop> Data { get; set; }
+
+    public IEnumerable<CofeeShop> LoadShops()
+    {
+        return new CofeeShop[]
+        {
+            new CofeeShop("adin",1),
+            new CofeeShop("dwa4",4),
+            new CofeeShop("semi",7)
+        };
+    }
+
+
+}
