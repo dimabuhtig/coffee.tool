20f154ac3e7fcbdf95ec27b91a5f0cccd6c5a72f
dimabuhtig <qa@qaqa.org>
2025-04-03 15:17:35 +0300

    er msg

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("q");
-Console.WriteLine("for exit");
-while (true)
+﻿internal class Program
 {
-    var command = Console.ReadLine();
-    if (string.Equals("q",command,StringComparison.OrdinalIgnoreCase)) break;
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("q");
+        Console.WriteLine("for exit");
+        while (true)
+        {
+            var command = Console.ReadLine();
+            if (string.Equals("q", command, StringComparison.OrdinalIgnoreCase)) break;
+            else { Console.WriteLine($"  it   is   er   {command} "); };
+        }
+    }
 }
